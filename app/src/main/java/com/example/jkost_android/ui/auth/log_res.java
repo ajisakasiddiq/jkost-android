8d26d0f50516951a78098664669664da2a3952c3
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-06 07:41:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jkost_android/ui/auth/log_res.java
@@ -0,0 +1,16 @@
+package com.example.jkost_android.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.jkost_android.R;
+
+public class log_res extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_res);
+    }
+}
