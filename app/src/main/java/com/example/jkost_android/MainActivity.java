a41ba6899039249779c76a5b7006c2518cbddb30
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-08 21:00:57 +0700

    Initial commit

--- app/src/main/java/com/example/jkost_android/MainActivity.java
+++ app/src/main/java/com/example/jkost_android/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.jkost_android;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.jkost_android.ui.fragment.AccountFragment;
+import com.example.jkost_android.ui.fragment.HomeFragment;
+import com.example.jkost_android.ui.fragment.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
@@ -22,39 +30,138 @@ import com.example.jkost_android.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView nav;
+    private int selectedTab = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nav = findViewById(R.id.nav);
 
-        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        final LinearLayout homeLayout = findViewById(R.id.homeLayout);
+        final LinearLayout searchLayout = findViewById(R.id.searchLayout);
+        final LinearLayout accountLayout = findViewById(R.id.accountLayout);
+
+        final ImageView homeImage = findViewById(R.id.homeImage);
+        final ImageView searchImage = findViewById(R.id.searchImage);
+        final ImageView accountImage = findViewById(R.id.accountImage);
+
+        final TextView homeTxt = findViewById(R.id.homeTxt);
+        final TextView searchTxt = findViewById(R.id.searchTxt);
+        final TextView accountTxt = findViewById(R.id.accountTxt);
+
+
+//        sethome
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragmentContainer, HomeFragment.class,null)
+                .commit();
+        homeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public void onClick(View v) {
+                if (selectedTab != 1){
+//        sethome
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer,HomeFragment.class,null)
+                            .commit();
+//                    unselect
+                    searchTxt.setVisibility(View.GONE);
+                    accountTxt.setVisibility(View.GONE);
 
-                switch (item.getItemId()){
+                    searchImage.setImageResource(R.drawable.search);
+                    accountImage.setImageResource(R.drawable.baseline_manage_accounts_24);
 
-                    case R.id.home:
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                        break;
+                    searchLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    accountLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+//                    select home tab
+                    homeTxt.setVisibility(View.VISIBLE);
+                    homeImage.setImageResource(R.drawable.home);
+                    homeLayout.setBackgroundResource(R.drawable.round_back_home_100);
+//                  create animation
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,0.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    homeLayout.startAnimation(scaleAnimation);
 
-                    case R.id.search:
-                        Toast.makeText(MainActivity.this, "Seacrh", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.akun:
-                        Toast.makeText(MainActivity.this, "Akun", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    default:
-
+                    selectedTab =1;
                 }
-                return true;
             }
         });
+
+
+        searchLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedTab != 2){
+//        sethome
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer, SearchFragment.class,null)
+                            .commit();
+//                    unselect
+                    homeTxt.setVisibility(View.GONE);
+                    accountTxt.setVisibility(View.GONE);
+
+                    homeImage.setImageResource(R.drawable.home);
+                    accountImage.setImageResource(R.drawable.baseline_manage_accounts_24);
+
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    accountLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+//                    select home tab
+                    searchTxt.setVisibility(View.VISIBLE);
+                    searchImage.setImageResource(R.drawable.search);
+                    searchLayout.setBackgroundResource(R.drawable.round_back_search_100);
+//                  create animation
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    searchLayout.startAnimation(scaleAnimation);
+
+                    selectedTab =2;
+                }
+            }
+        });
+
+
+        accountLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedTab != 3){
+
+                    //        sethome
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer, AccountFragment.class,null)
+                            .commit();
+//                    unselect
+                    homeTxt.setVisibility(View.GONE);
+                    searchTxt.setVisibility(View.GONE);
+
+
+                    homeImage.setImageResource(R.drawable.home);
+                    searchImage.setImageResource(R.drawable.baseline_notifications_24);
+
+
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    searchLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+//                    select home tab
+                    accountTxt.setVisibility(View.VISIBLE);
+                    accountImage.setImageResource(R.drawable.search);
+                    accountLayout.setBackgroundResource(R.drawable.round_back_account_100);
+//                  create animation
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    accountLayout.startAnimation(scaleAnimation);
+
+                    selectedTab = 4;
+                }
+            }
+        });
+
 
     }
 }
