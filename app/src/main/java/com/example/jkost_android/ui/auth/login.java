7b694d7f78c08b089b72794e7a13369b2e5ac3e8
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-13 14:12:14 +0700

    Initial commit

--- app/src/main/java/com/example/jkost_android/ui/auth/login.java
+++ app/src/main/java/com/example/jkost_android/ui/auth/login.java
@@ -1,12 +1,12 @@
 package com.example.jkost_android.ui.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -19,17 +19,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class login extends AppCompatActivity {
-    private EditText email, password;
+    private EditText etEmail, etPassword;
     private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+        etEmail = findViewById(R.id.email);
+        etPassword = findViewById(R.id.password);
         btnLogin = findViewById(R.id.btnlogin);
-
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,42 +39,39 @@ public class login extends AppCompatActivity {
         });
     }
 
+    private void login() {
+        final String email = etEmail.getText().toString().trim();
+        final String password = etPassword.getText().toString().trim();
 
-        private void login() {
-            final String email = email.getText().toString().trim();
-            final String password = password.getText().toString().trim();
+        String url = "http://your-api-url/login.php";
 
-            String url = "http://your-api-url/login.php";
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            if (response.equals("success")) {
-                                // Login berhasil
-                                Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
-                                // Tambahkan kode untuk pindah ke halaman utama aplikasi setelah login berhasil
-                            } else {
-                                // Login gagal
-                                Toast.makeText(login.this, "Login failed", Toast.LENGTH_SHORT).show();
-                            }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.equals("success")) {
+                            // Login berhasil
+                            Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            // Tambahkan kode untuk pindah ke halaman utama aplikasi setelah login berhasil
+                        } else {
+                            // Login gagal
+                            Toast.makeText(login.this, "Login failed", Toast.LENGTH_SHORT).show();
                         }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(login.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("email", email);
-                    params.put("password", password);
-                    return params;
-                }
-            };
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(login.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
 
-            VolleySingleton
-    }
-    }
+    }}
