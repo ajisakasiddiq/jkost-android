d073c076891d0fcbcf9cea28047339f1ffa9ad7e
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-13 13:57:09 +0700

    Initial commit

--- app/src/main/java/com/example/jkost_android/ui/auth/login.java
+++ app/src/main/java/com/example/jkost_android/ui/auth/login.java
@@ -3,18 +3,78 @@ package com.example.jkost_android.ui.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.jkost_android.R;
+import com.example.jkost_android.VolleySingleton;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
-
+    private EditText email, password;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnlogin);
 
 
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
-}
+
+
+        private void login() {
+            final String email = email.getText().toString().trim();
+            final String password = password.getText().toString().trim();
+
+            String url = "http://your-api-url/login.php";
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("success")) {
+                                // Login berhasil
+                                Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                // Tambahkan kode untuk pindah ke halaman utama aplikasi setelah login berhasil
+                            } else {
+                                // Login gagal
+                                Toast.makeText(login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(login.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("email", email);
+                    params.put("password", password);
+                    return params;
+                }
+            };
+
+            VolleySingleton
+    }
+    }
