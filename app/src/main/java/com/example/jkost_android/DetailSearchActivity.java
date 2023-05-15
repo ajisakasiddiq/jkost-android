704c014d480a62b0faeaddf425ef075a856481b0
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-15 09:41:45 +0700

    Initial commit

--- app/src/main/java/com/example/jkost_android/DetailSearchActivity.java
+++ app/src/main/java/com/example/jkost_android/DetailSearchActivity.java
@@ -9,6 +9,6 @@ public class DetailSearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_search);
+        setContentView(R.layout.activity_search2);
     }
 }
---
+++ app/src/main/java/com/example/jkost_android/ui/auth/RegisterActivity.java
@@ -0,0 +1,82 @@
+package com.example.jkost_android.ui.auth;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jkost_android.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String REGISTER_URL = "http://10.10.5.45:8000/api/register";
+
+    private EditText etname;
+    private EditText etEmail;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etname = findViewById(R.id.et_name);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        final String name = etname.getText().toString().trim();
+        final String email = etEmail.getText().toString().trim();
+        final String password = etPassword.getText().toString().trim();
+
+        // Buat request POST ke URL REGISTER_URL
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Tanggapan dari server jika pendaftaran berhasil
+                        Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Tanggapan dari server jika terjadi kesalahan
+                        Toast.makeText(RegisterActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        // Buat antrian permintaan Volley dan tambahkan permintaan ke antrian
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+}
