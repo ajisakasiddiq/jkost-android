23724c1e6c7e9b6d87108651205d0f7466e5e4b3
zidan <zidanprasetyo536@gmail.com>
2023-05-17 11:06:34 +0700

    commit uhuy

--- app/src/main/java/com/example/jkost_android/ui/fragment/AccountFragment.java
+++ app/src/main/java/com/example/jkost_android/ui/fragment/AccountFragment.java
@@ -35,7 +35,6 @@ public class AccountFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AccountFragment.
      */
     // TODO: Rename and change types and number of parameters
