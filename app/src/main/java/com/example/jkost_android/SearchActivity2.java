57c9a380e2f305db63012ba5b06a406ab6531c1a
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-06-11 12:13:27 +0700

    Initial commit

--- app/src/main/java/com/example/jkost_android/SearchActivity2.java
+++ app/src/main/java/com/example/jkost_android/SearchActivity2.java
@@ -6,72 +6,103 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jkost_android.util.UtilApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity2 extends AppCompatActivity {
-
-    SearchView searchView;
-    RecyclerView recyclerView;
-    ArrayList<ModelClass> arrayList=new ArrayList<>();
-    ArrayList<ModelClass> searchList;
-    String[] kostList=new String[]{"Kost Cantik, Kost Kafka, Kost Putra Oyi, Kost Orchid, Kost Putri Cinderella"};
-
-    String[] kostNum=new String[]{"Kost 1", "Kost 2", "Kost 3", "Kost 4", "Kost 5"};
-
-    int[] imgList=new int[]{R.drawable.kost1,R.drawable.kost2,R.drawable.kost3,R.drawable.kost4,R.drawable.kost5,};
+    private EditText searchEditText;
+    private Button searchButton;
+    private RecyclerView recyclerView;
+    private SearchAdapter searchAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search2);
-        recyclerView=findViewById(R.id.recycleView);
-        searchView=findViewById(R.id.searchView);
 
-        for (int i = 0; i<kostList.length ; i++) {
-            ModelClass modelClass = new ModelClass();
-            modelClass.setNamakost(kostList[i]);
-            modelClass.setHarga(kostNum[i]);
-            modelClass.setImgpertama(String.valueOf(imgList[i]));
-            arrayList.add(modelClass);
-        }
+//        searchEditText = findViewById(R.id.searchEditText);
+//        searchButton = findViewById(R.id.searchButton);
+//        recyclerView = findViewById(R.id.recyclerView);
 
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(SearchActivity2.this);
-        recyclerView.setLayoutManager(layoutManager);
+        // Inisialisasi adapter dan RecyclerView
+        searchAdapter = new SearchAdapter(SearchActivity2.this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(searchAdapter);
 
-        KostAdapter kostAdapter=new KostAdapter(SearchActivity2.this,arrayList);
-        recyclerView.setAdapter(kostAdapter);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchList=new ArrayList<>();
-
-                if (query.length()>0){
-                    for (int i = 0; i <arrayList.size() ; i++) {
-                        if (arrayList.get(i).getNamakost().toUpperCase().contains(query.toUpperCase())){
-
-                            ModelClass modelClass = new ModelClass();
-                            modelClass.setNamakost(arrayList.get(i).getNamakost());
-//                            modelClass.setHarga(arrayList.get(i).getHarga(data.getString("harga")));
-//                            modelClass.setImgpertama(Integer.parseInt(arrayList.get(i).setImgpertama()));
-                            searchList.add(modelClass);
-                        }
-                    }
-                    RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(SearchActivity2.this);
-                    recyclerView.setLayoutManager(layoutManager);
-
-                    KostAdapter kostAdapter=new KostAdapter(SearchActivity2.this,arrayList);
-                    recyclerView.setAdapter(kostAdapter);
-
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
+            public void onClick(View v) {
+                String query = searchEditText.getText().toString().trim();
+                performSearch(query);
             }
         });
     }
+
+    private void performSearch(String query) {
+        String url = "http://"+ UtilApi.API_URL  + "/api/search/"  + query;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Tangani respons pencarian yang diterima
+                        List<ModelClass> kamarList = parseSearchResults(response);
+//                        searchAdapter.setData(kamarList);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Tangani kesalahan yang terjadi saat melakukan pencarian
+                        Toast.makeText(SearchActivity2.this, "Terjadi kesalahan: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Tambahkan permintaan ke antrian permintaan Volley
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    private List<ModelClass> parseSearchResults(JSONObject response) {
+        // Lakukan parsing dan konversi respons JSON ke objek List<Kamar>
+        // Anda dapat menyesuaikan kode ini sesuai dengan struktur respons JSON dari server
+
+        List<ModelClass> kamarList = new ArrayList<>();
+
+        try {
+            JSONArray resultsArray = response.getJSONArray("results");
+
+            for (int i = 0; i < resultsArray.length(); i++) {
+                JSONObject kamarObject = resultsArray.getJSONObject(i);
+
+                int id = kamarObject.getInt("id");
+                String nama = kamarObject.getString("nama");
+                String lokasi = kamarObject.getString("lokasi");
+                // ... lanjutkan dengan mendapatkan data lainnya sesuai dengan struktur JSON
+
+//                Kamar kamar = new Kamar(id, nama, lokasi);
+//                kamarList.add(kamar);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return kamarList;
+    }
 }
+
