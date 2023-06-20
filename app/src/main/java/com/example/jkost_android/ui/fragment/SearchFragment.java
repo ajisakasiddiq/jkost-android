3202336d7079d963ccc26a076eb0b44510163bb8
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-06-20 18:25:40 +0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/jkost_android/ui/fragment/SearchFragment.java
+++ app/src/main/java/com/example/jkost_android/ui/fragment/SearchFragment.java
@@ -1,14 +1,37 @@
 package com.example.jkost_android.ui.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jkost_android.AdapterRiwayat;
+import com.example.jkost_android.ClassModel;
+import com.example.jkost_android.KostAdapter;
+import com.example.jkost_android.ModelClass;
 import com.example.jkost_android.R;
+import com.example.jkost_android.util.UtilApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +39,15 @@ import com.example.jkost_android.R;
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
+
+
+    private RecyclerView recyclerView;
+    private KostAdapter kamarAdapter;
+    private ArrayList<ModelClass> arrayList;
+    private ModelClass modelClass;
+    private EditText editTextSearch;
+    private Button buttonSearch;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,10 +89,70 @@ public class SearchFragment extends Fragment {
         }
     }
 
+//    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_search2, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        arrayList = new ArrayList<ModelClass>();
+        kamarAdapter = new KostAdapter(getContext(), arrayList);
+        recyclerView.setAdapter(kamarAdapter);
+        editTextSearch = view.findViewById(R.id.editTextSearch);
+        buttonSearch = view.findViewById(R.id.buttonSearch);
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performSearch();
+            }
+        });
+
+
+        return view;
+    }
+
+
+    private void performSearch() {
+        // Ganti dengan URL endpoint API Anda
+        String query = editTextSearch.getText().toString();
+        String url = "http://"+ UtilApi.API_URL  + "/api/search?nama_kost=" +query;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                jsonObject = jsonArray.getJSONObject(i);
+                                modelClass = new ModelClass();
+                                modelClass.setNamakost(jsonObject.getString("nama_kost"));
+                                modelClass.setHarga(jsonObject.getString("harga"));
+                                modelClass.setNo_kamar(jsonObject.getString("no_kamar"));
+//                                modelClass.setStatus(jsonObject.getString("status"));
+                                SearchFragment.this.arrayList.add(modelClass);
+                            }
+
+                            kamarAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
     }
 }
